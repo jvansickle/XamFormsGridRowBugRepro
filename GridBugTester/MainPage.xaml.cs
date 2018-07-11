c3b1e0a925459016490349889b55c31503c3b383
John VanSickle <jvansickle@ceiamerica.com>
2018-07-11 10:05:45 -0400

    Initial commit

---
+++ GridBugTester/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridBugTester
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Show_Clicked(object sender, System.EventArgs e)
+        {
+            testGrid.RowDefinitions[1].Height = 15;
+            testGrid.HeightRequest += 15;
+        }
+
+        void Handle_Hide_Clicked(object sender, System.EventArgs e)
+        {
+            testGrid.RowDefinitions[1].Height = 0;
+            testGrid.HeightRequest -= 15;
+        }
+    }
+}
